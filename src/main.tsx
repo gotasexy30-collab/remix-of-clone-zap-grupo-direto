9ad9d6d487e78e6fd9901a929c430126e1cc8b98
gotasexy30-collab <gotasexy30@gmail.com>
2026-09-15 23:19:02 -0300

    fix: inicializar Meta Pixel ao carregar a aplicação

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { initMetaPixel } from "./services/pixel";
 import "./index.css";
+
+void initMetaPixel();
 
 createRoot(document.getElementById("root")!).render(<App />);
